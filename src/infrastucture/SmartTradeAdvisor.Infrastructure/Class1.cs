beba8c4972b45f5128a3d0a666055ce787f035ae
bubus128 <j.swislocki@outlook.com>
2023-12-03 10:48:17 +0100

    Create initial sollution structure

---
+++ src/infrastucture/SmartTradeAdvisor.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartTradeAdvisor.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/tests/SmartTradeAdvisor.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace SmartTradeAdvisor.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

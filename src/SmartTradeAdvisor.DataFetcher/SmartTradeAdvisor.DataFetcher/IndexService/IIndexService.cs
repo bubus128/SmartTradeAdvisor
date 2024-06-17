1925dc7df6483275def0cc487a9ef2a866cb04c5
bubus128 <j.swislocki@outlook.com>
2024-06-17 17:13:56 +0200

    Update data fetcher

--- src/SmartTradeAdvisor.DataFetcher/SmartTradeAdvisor.DataFetcher/IndexService/IIndexService.cs
+++ src/SmartTradeAdvisor.DataFetcher/SmartTradeAdvisor.DataFetcher/IndexService/IIndexService.cs
@@ -4,20 +4,20 @@ namespace SmartTradeAdvisor.DataFetcher.IndexService;
 
 public interface IIndexService
 {
-    public Task<DateTime?> GetLatestRecordDate(string symbol);
+    public DateTime? GetLatestRecordDate(string symbol);
 
-    public Task<bool> CheckSymbolExists(string symbol);
+    public bool CheckSymbolExists(string symbol);
 
-    public Task CreateSymbol(MarketIndex symbol);
+    public void CreateSymbol(MarketIndex symbol);
 
-    public Task<List<MarketIndexValue>> FetchDataFromNasdaq(string symbol, DateTime startDate, DateTime endDate);
+    public List<MarketIndexValue> FetchDataFromNasdaq(string symbol);
 
-    public Task<List<MarketIndexValue>> FetchDataFromBinance(string symbol, DateTime startDate, DateTime endDate);
+    public List<MarketIndexValue> FetchDataFromBinance(string symbol, DateTime startDate, DateTime endDate);
 
 
-    public Task SendValuesToSmartTradeAdvisor(List<MarketIndexValue> marketIndexValues, string symbol);
+    public void SendValuesToSmartTradeAdvisor(List<MarketIndexValue> marketIndexValues, string symbol);
 
-    public Task SendValueToSmartTradeAdvisor(MarketIndexValue marketIndexValue, string symbol);
+    public void SendValueToSmartTradeAdvisor(MarketIndexValue marketIndexValue, string symbol);
 
     public string ParseData(string nasdaqData);
 }
